fc0bdf50ea1ccc9abf1234b3522d2789b2d149bd
Maki <m@sutty.nl>
2023-05-23 21:23:57 -0300

    punto 4: promedio entre 3 números

---
+++ practico0/promedio3.js
@@ -0,0 +1,21 @@
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+let promedio = (num1 + num2 + num3) / 3;
+
+console.log("El promedio entre", num1 + ", " + num2 + " y " +  num3, "es:", promedio);
+
+num1 = -2;
+num2 = -3;
+num3 = -8;
+promedio = (num1 + num2 + num3) / 3;
+
+console.log("El promedio entre", num1 + ", " + num2 + " y " +  num3, "es:", promedio);
+
+num1 = -1.2;
+num2 = 5.3;
+num3 = -3.3;
+promedio = (num1 + num2 + num3) / 3;
+
+console.log("El promedio entre", num1 + ", " + num2 + " y " +  num3, "es:", promedio);
+
