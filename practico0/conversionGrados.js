ae5acf4aaf3feff63300a362cb72fc30627691ed
Maki <m@sutty.nl>
2023-05-23 21:15:44 -0300

    punto 3: conversión de grados fahrenheit a celsius

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,10 @@
+let fahrenheit1 = 50;
+let fahrenheit2 = 75;
+
+let celsius1 = (fahrenheit1 - 32) * 5/9;
+let celsius2 = (fahrenheit2 - 32) * 5/9;
+
+console.log("El equivalente de", fahrenheit1, "grados fahrenheit es", celsius1, "grados celsius.");
+
+console.log("El equivalente de", fahrenheit2, "grados fahrenheit es", celsius2, "grados celsius.");
+
