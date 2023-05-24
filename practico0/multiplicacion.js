85310c66b0fb83a1c766b1b32a458eac8fb1459f
Maki <m@sutty.nl>
2023-05-23 21:03:03 -0300

    punto uno: resta y multiplicación

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,6 @@
+let num1 = 2;
+let num2 = 17;
+let num3 = 6;
+let producto = num1 * num2 * num3;
+
+console.log(producto);
---
+++ practico0/resta.js
@@ -0,0 +1,5 @@
+let num1 = 10;
+let num2 = 2;
+let resta = num1 - num2;
+
+console.log(resta);
