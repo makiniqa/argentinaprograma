d1e3b983e55fa747c896f042582d979ff27bf70a
Maki <m@sutty.nl>
2023-05-23 21:11:26 -0300

    punto 2: área de un rectángulo

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,11 @@
+let base1 = 5;
+let altura1 = 3;
+let area1 = base1 * altura1;
+
+let base2 = 12.5;
+let altura2 = 5.3;
+let area2 = base2 * altura2;
+
+console.log("El área de un rectángulo de", base1, "cm de base y", altura1, "cm de altura es:", area1);
+
+console.log("El área de un rectángulo de", base2, "cm de base y", altura2, "cm de altura es:", area2);
