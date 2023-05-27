5e0da998d140597d725a21da570d3fe0b63bbc78
Maki <m@sutty.nl>
2023-05-27 10:25:57 -0300

    fix: faltaba un punto y coma

--- practico0-extra/promedio6.js
+++ practico0-extra/promedio6.js
@@ -4,7 +4,7 @@ let promedio = (num_array[1] + num_array[1] + num_array[2]) / 3;
 
 console.log("El promedio entre", num_array[0] + ", " + num_array[1] + " y " +  num_array[2], "es:", promedio);
 
-num_array.push(4,3,1)
+num_array.push(4,3,1);
 
 promedio = (num_array[1] + num_array[1] + num_array[2] + num_array[3] + num_array[4] + num_array[5]) / 6;
 
