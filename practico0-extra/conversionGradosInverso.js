7eab1bdf3d7a4ca9d4766b8b3be462ccfef56488
Maki <m@sutty.nl>
2023-05-27 10:29:10 -0300

    punto 2: celsius a fahrenheit

---
+++ practico0-extra/conversionGradosInverso.js
@@ -0,0 +1,9 @@
+let celsius1 = 20;
+let celsius2 = 25;
+
+let fahrenheit1 = celsius1 * 9/5 + 32;
+let fahrenheit2 = celsius2 * 9/5 + 32;
+
+console.log("El equivalente de", celsius1, "grados celsius es", fahrenheit1, "grados fahrenheit.");
+
+console.log("El equivalente de", celsius2, "grados celsius es", fahrenheit2, "grados fahrenheit.");
