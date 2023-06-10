c0777eb7fd1b39f39cb6d56664549ea1bf70b226
Maki <m@sutty.nl>
2023-06-10 10:31:41 -0300

    parte 2: ej 5

---
+++ practico1/frutas.js
@@ -0,0 +1,9 @@
+let frutas = [];
+
+frutas.push("kinoto", "pitahaya", "frambuesa", "carambola", "manzana");
+
+let ultima = frutas[frutas.length - 1];
+let primera = frutas [0];
+
+console.log("La última fruta agregada fue:", ultima);
+console.log("La primera fruta agregada fue:", primera);
