806ce10fbffeb06a32f4a6383f47170414550862
Maki <m@sutty.nl>
2023-06-22 19:19:58 -0300

    fix: faltaban punto y comas

--- practico2/positivoNegativo.js
+++ practico2/positivoNegativo.js
@@ -2,9 +2,9 @@ const readlineSync = require('readline-sync');
 let num = readlineSync.question("Ingresá un número: ");
 
 if (num == 0) {
-    console.log("El número es cero")
+    console.log("El número es cero");
 } else if (num > 0) {
-    console.log("El número es positivo")
+    console.log("El número es positivo");
 } else {
-    console.log("El número es negativo")
-}
+    console.log("El número es negativo");
+};
