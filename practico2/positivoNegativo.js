9cc980f432afce04dca86b7cff6ff4f4fe2ba2b6
Maki <m@sutty.nl>
2023-06-22 18:50:27 -0300

    punto 1b: determinar si el número ingresado es positivo, negativo o cero

---
+++ practico2/positivoNegativo.js
@@ -0,0 +1,10 @@
+const readlineSync = require('readline-sync');
+let num = readlineSync.question("Ingresá un número: ");
+
+if (num == 0) {
+    console.log("El número es cero")
+} else if (num > 0) {
+    console.log("El número es positivo")
+} else {
+    console.log("El número es negativo")
+}
