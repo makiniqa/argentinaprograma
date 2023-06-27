cadc2afe610c6217cf2742f6df708f05c85cf85a
Maki <m@sutty.nl>
2023-06-26 22:01:41 -0300

    punto 2: lista de invitados

---
+++ practico3/listaDeInvitados.js
@@ -0,0 +1,21 @@
+personas = ["Jose", "Maria", "Juan", "Andres", "Lionel", "Sofia", "Leandro", "Emilia"];
+admitidos = [];
+rechazados = [];
+
+for (let i = 0; i < personas.length; i++) {
+    if (personas[i] == "Jose" || personas[i] == "Sofia") {
+        rechazados.push(personas[i]);
+    } else {
+        admitidos.push(personas[i]);
+    };
+};
+
+console.log("La lista de invitados admitidos es: ")
+for (let admitido = 0; admitido < admitidos.length; admitido++) {
+    console.log(admitidos[admitido]);
+};
+
+console.log("La lista de invitados rechazados es: ")
+for (let rechazado = 0; rechazado < rechazados.length; rechazado++) {
+    console.log(rechazados[rechazado]);
+};
